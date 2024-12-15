780ba5e6d14622a8f933f9c93ff46df0ae5a235e
Antonio Pizarro <apizarrocotes@outlook.es>
2024-12-15 19:18:37 +0100

    Project  setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MlPipeline",
+    version= "0.0.0",
+    author="APC",
+    author_email="apizarrocotes@outlook.es",
+    packages= find_packages(),
+    install_requires=[]
+)
